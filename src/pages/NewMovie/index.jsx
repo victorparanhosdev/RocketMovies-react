199de4963b4b466ccaa6b0f4ad5ef3d583bec823
Victor Paranhos <victor_paranhos@hotmail.com>
2023-03-21 09:17:40 -0300

    add newmovie

--- src/pages/NewMovie/index.jsx
+++ src/pages/NewMovie/index.jsx
@@ -1,42 +1,51 @@
-import { Container, Content } from "./styles"
+import { Container, Content, Text } from "./styles"
 import { Header } from "../../Componentes/Header"
 import { ButtonText } from "../../Componentes/ButtonText"
-import {FiArrowLeft} from "react-icons/fi"
+import { FiArrowLeft } from "react-icons/fi"
+
 import { Input } from "../../Componentes/Input"
 import { Button } from "../../Componentes/Button"
-export function NewMovie(){
-  return(
+import { Tag } from "../../Componentes/Tag"
+import {IoMdAdd, IoMdClose} from "react-icons/io"
+
+export function NewMovie() {
+  return (
     <Container>
       <Header />
-    
+
       <Content>
-      <ButtonText to="/" icon={FiArrowLeft} name="Voltar"/>
-      <main>
-        <h1>Novo Filme</h1>
-        <div>
-        <Input type="text" placeholder="Titulo"/>
-        <Input type="text" placeholder="Sua nota (de 0 a 5)"/>
-        </div>
-        
-        <textarea placeholder="Observação" name="" id="" rows="5"></textarea>
- 
-        <div>
-          <h2>Marcadores</h2>
+        <ButtonText to="/" icon={FiArrowLeft} name="Voltar" />
+        <main>
+          <h1>Novo Filme</h1>
+
+          <Text>
+
+            <div>
+              <Input type="text" placeholder="Titulo" />
+              <Input type="text" placeholder="Sua nota (de 0 a 5)" />
+            </div>
+
+            <textarea placeholder="Observação" name="" id="" rows="4"></textarea>
+          </Text>
+
           <div>
-            <span>React</span>
+            <h2>Marcadores</h2>
+            <div>
+              <span>React<IoMdClose/></span>
+              <span>Novo Marcador<IoMdAdd/></span>
+            </div>
+
           </div>
-          
-        </div>
 
-        <div>
-        <Button name="Excluir filme" />
-        <Button name="Salvar alterações" />
-        </div>
-       
+          <div>
+            <Button name="Excluir filme" />
+            <Button name="Salvar alterações" />
+          </div>
 
 
 
-      </main>
+
+        </main>
       </Content>
     </Container>
   )
--- src/pages/NewMovie/styles.js
+++ src/pages/NewMovie/styles.js
@@ -1,82 +1,120 @@
-import styled from "styled-components";
+import styled from 'styled-components'
 
-export const Container = styled.div`
+export const Container = styled.div``
 
+export const Text = styled.div`
 
-`;
-
-export const Content = styled.div`
-
-max-width: 140rem;
-margin: 4rem auto 0;
-
-main {
-  height: calc(100vh - 20rem);
-  overflow-y: auto;
-  
   textarea {
-    background: ${({theme})=> theme.COLORS.BACKGROUND_200};
+    background: ${({ theme }) => theme.COLORS.BACKGROUND_200};
     padding: 1.9rem 1.6rem;
     border-radius: 1rem;
     margin: 4rem auto;
     border: none;
     width: 100%;
     font-size: 1.6rem;
-    color: ${({theme})=> theme.COLORS.WHITE};
+    color: ${({ theme }) => theme.COLORS.WHITE};
     border: 2px solid transparent;
     transition: border 0.3s;
     outline: none;
     resize: none;
 
-    &:focus{
-      border: 2px solid ${({theme})=> theme.COLORS.ROSE};
-
+    &:focus {
+      border: 2px solid ${({ theme }) => theme.COLORS.ROSE};
     }
 
-    &::placeholder{
-      font-family: "Roboto";
+    &::placeholder {
+      font-family: 'Roboto';
       font-size: 1.6rem;
-      color: ${({theme})=> theme.COLORS.GRAY_200};
+      color: ${({ theme }) => theme.COLORS.GRAY_200};
       font-weight: 400;
     }
-
-  }
-  > h1 {
-    font-size: 3.6rem;
-    font-weight: 500;
-    font-family: "Roboto Slab";
-    margin: 2.4rem auto 4rem;
   }
 
   > div:has(input) {
     display: flex;
     gap: 4rem;
+  }
+`
+
+export const Content = styled.div`
+  max-width: 140rem;
+  margin: 4rem auto 0;
+
+  main {
+
+  
+    > h1 {
+      font-size: 3.6rem;
+      font-weight: 500;
+      font-family: 'Roboto Slab';
+      margin: 2.4rem auto 4rem;
+    }
+    > div:has(span) {
+
+      h2 {
+        color: ${({ theme }) => theme.COLORS.TEXT};
+        font-size: 2rem;
+        font-weight: 400;
+      }
+
+      > div {
+        margin-top: 2.4rem;
+        padding: 1.6rem;
+        width: 100%;
+        background-color: #0d0c0f;
+        display: flex;
+        align-items: center;
+        gap: 2.4rem;
+        border-radius: 0.8rem;
+        svg {
+          width: 2.4rem;
+          height: 2.4rem;
+          color: ${({ theme }) => theme.COLORS.ROSE};
+          cursor: pointer;
+        }
+
+        
+        span:first-child {
+          font-size: 1.6rem;
+          font-family: "Roboto";
+          color: #FFFFFF;
+          font-weight: 400;
+          padding: 1.6rem;
+          background:${({ theme }) => theme.COLORS.BACKGROUND_200};
+          border-radius: 1rem;
+          display: flex;
+          align-items: center;
+          gap: 1.6rem;    
+        }
+
+        span:last-child {
+          color: ${({theme})=> theme.COLORS.GRAY_200};
+          border: 2px dashed;
+          font-weight: 400;
+          padding: 1.6rem;
+          border-radius: 1rem;
+          display: flex;
+          align-items: center;
+          gap: 1.6rem;
+
+       
     
+        }
+      }
+    }
   }
 
-  > div:has(span) {
-
-    background-color: #0D0C0F;
-    
+  > a {
+    justify-content: start;
   }
 
-}
-
-
-> a {
-  justify-content: start;
-}
-
-div:has(a){
-  display: flex;
-  gap: 4rem;
-  margin: 4rem auto;
- a:first-child {
-  background-color: #0D0C0F;
-  color: #FF859B;
- }
-}
-
-
-
-`;
+  div:has(a) {
+    display: flex;
+    gap: 4rem;
+    margin: 4rem auto;
+    a:first-child {
+      background-color: #0d0c0f;
+      color: #ff859b;
+    }
+  }
+`
