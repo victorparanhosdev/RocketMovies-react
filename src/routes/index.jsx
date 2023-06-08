38796f4bf17baa11cbdaebcad2a10a084d141345
Victor Paranhos <victor_paranhos@hotmail.com>
2023-06-07 22:19:34 -0300

    Update index.jsx

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -6,7 +6,7 @@ import { UserRoutes } from './user.routes';
 export function Routes() {
     return(
         <BrowserRouter>
-        <UserRoutes/>
+        <AuthRoutes/>
         </BrowserRouter>
     )
 }
